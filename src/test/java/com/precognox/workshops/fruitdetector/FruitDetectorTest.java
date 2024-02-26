85b2c02fdc4d548522f0e84f5e99357ebad6b627
Tamas Csizmadia <tamas@me.com>
2024-02-26 09:52:07 +0100

    chore: organize imports
    Remove unneeded import

--- src/test/java/com/precognox/workshops/fruitdetector/FruitDetectorTest.java
+++ src/test/java/com/precognox/workshops/fruitdetector/FruitDetectorTest.java
@@ -1,6 +1,5 @@
 package com.precognox.workshops.fruitdetector;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
